65471383e0eda56bd61bc563f37460ab79989e46
Jay Edwards <jay@sentenai.com>
2018-02-22 15:46:29 -0500

    Ignore dbs. Use subdir for dbs

--- mkdb.sh
+++ mkdb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sqlite3 links.db<<EOF
+sqlite3 db/links.db<<EOF
 CREATE TABLE links (id ROWID, name TEXT UNIQUE, url TEXT UNIQUE, hits INT DEFAULT 0, created_at TEXT);
 CREATE INDEX links_hits on links(hits);
 EOF
